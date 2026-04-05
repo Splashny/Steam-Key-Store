a48c8950ab8f417d561076cc0b44654a408874b0
Splashny <119583500+Splashny@users.noreply.github.com>
2026-04-05 12:58:43 +0500

    url

--- scripts/dropdown.js
+++ scripts/dropdown.js
@@ -2,6 +2,7 @@ const menuBtn = document.getElementById("menuBtn");
 const dropdownMenu = document.getElementById("dropdownMenu");
 
 menuBtn.addEventListener("click", () => {
+      console.log("КНОПКА НАЖАТА");
     dropdownMenu.classList.toggle("active");
 });
 
